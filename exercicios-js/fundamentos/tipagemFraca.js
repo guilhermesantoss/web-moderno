072ad5cd3ceaa2e403528d5a341842e790b5fc6f
Guilherme Santos TED CommandLine <guilherme.santos@tlsbr.com>
2019-03-25 15:13:13 -0300

    add aula Tipagem Fraca

---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+// verifica qual o tipo da variavel
+console.log(typeof qualquer);
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nomes genéricos e siglas para criação de variáveis
+let valor = '';
+let numero = 1;
+let pqp = false; // Produto Químico Perigoso... kkk
