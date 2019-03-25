e00c0373221278600431c5841034795f58fa465c
Guilherme Santos TED CommandLine <guilherme.santos@tlsbr.com>
2019-03-25 10:39:32 -0300

    add arquivos

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+console.log("Sentença de código");
+
+// um bloco obrigatoriamente sempre vai estar dentro um do outro
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso        
+    }
+}
+
+// javascript é organizado em sentenças de código que podem ou não terminar com ponto e vírgula
+// e é organizado em blocos de código, esses blocos são delimitados por chaves
