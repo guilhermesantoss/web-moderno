8caffa025d3a9f58f0576fb7c958d21c5251e3fa
Guilherme Santos TED CommandLine <guilherme.santos@tlsbr.com>
2019-03-25 16:34:05 -0300

    add aula Tipos em JavaSCript: String

---
+++ exercicios-js/fundamentos/strings.js
@@ -0,0 +1,16 @@
+const escola = "Cod3r";
+
+console.log(escola.charAt(4));
+console.log(escola.charAt(5)); // imprimi o indice 
+console.log(escola.charCodeAt(3)); // código da tabela unicode html
+console.log(escola.indexOf('3'));
+
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+
+console.log('Escola '.concat(escola).concat("!"));
+console.log(escola.replace(3, 'e'));
+
+console.log('Ana,Maria,Pedro,Guilherme'.split(','));
+
+
