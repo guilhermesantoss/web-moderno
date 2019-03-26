a37031ad27d268a407f2071a09396ef65ff635c3
Guilherme Lopes dos Santos <g.santos.sfc@gmail.com>
2019-03-25 23:08:04 -0300

    add aula Declaracao de Variaveis com Var #02

---
+++ exercicios-js/fundamentos/usandoVar2.js
@@ -0,0 +1,10 @@
+var numero = 1;
+
+{
+    var numero = 2;
+    console.log('dentro = ', numero);
+}
+
+console.log('fora = ', numero);
+
+// uma variavel declarada com var, soh eh visivel globalmente em escopos que sejam diferentes de uma funcao
