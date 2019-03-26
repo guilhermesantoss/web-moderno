aed83953f84070bc22221182910c08e332cc1c3f
Guilherme Santos TED CommandLine <guilherme.santos@tlsbr.com>
2019-03-26 15:32:15 -0300

    add aula Operadores Aritimeticos

---
+++ exercicios-js/fundamentos/aritimeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+// operadores binarios
+const soma = a + b + c + d;
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+const modulo = a % 2;
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
+
+// exemplo de operador unario
+console.log(-divisao);
