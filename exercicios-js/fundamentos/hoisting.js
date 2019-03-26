c3571d21c9302b764592c89be00041a011302e0f
Guilherme Santos TED CommandLine <guilherme.santos@tlsbr.com>
2019-03-26 10:13:50 -0300

    add aula Entendendo o Hoisting

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,9 @@
+console.log('a = ', a);
+var a = 2;
+console.log('a = ', a);
+
+console.log('b = ', b);
+let b = 2;
+console.log('b = ', b);
+
+// conceito de hoisting é jogar as declaracoes das variaveis definidas com var para o topo do codigo
