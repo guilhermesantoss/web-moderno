b12fc7faa4c7f9a1473840f9a9c0a53c7ab59f28
Guilherme Lopes dos Santos <g.santos.sfc@gmail.com>
2019-03-25 23:02:59 -0300

    add aula Declaracao de Variaveis com Var #01

---
+++ exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,22 @@
+{
+    {
+        {
+            {
+                var sera = 'Sera???'
+                console.log(sera);
+            }
+        }
+    }
+}
+
+console.log(sera);
+
+function teste() {
+    var local = 123;
+    console.log(local);
+}
+
+teste();
+console.log(local);
+
+// Sempre que vc cria uma variavel no escopo global, ela automaticamente pertence ao objeto window, falando somente de browser
