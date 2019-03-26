54fa2a195c8af0d55b2c3d70e463bd67a53e1a81
Guilherme Lopes dos Santos <g.santos.sfc@gmail.com>
2019-03-25 23:39:48 -0300

    add aula Declaracao de Variaveis com Let

---
+++ exercicios-js/fundamentos/usandoLet1.js
@@ -0,0 +1,11 @@
+let numero = 1;
+{
+    let numero = 2;
+    console.log('dentro = ', numero);
+}
+
+console.log('fora = ', numero);
+
+// variaveis criadas com a palavra reservada var, tem escopo global e de funcao
+
+// as variaveis criadas com a palvra reservada let, tem escopo global, de funcao e de bloco
