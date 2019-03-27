5e925d45d0beb8783df7e05f336d80f0f67900f9
Guilherme Santos TED CommandLine <guilherme.santos@tlsbr.com>
2019-03-27 18:01:19 -0300

    add aula Cidadao de Primeira Linha

---
+++ exercicios-js/funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,36 @@
+// Funcao em JS eh First-Class Object (Citizens)
+// Higher-order function
+
+// criar funcao de forma literal
+function fun1() {}
+
+// Armazenar em uma variavel - funcao anonima
+const fun2 = function() {}
+
+// Armazenar em um array
+const array = [function (a, b) { return a + b;}, fun1, fun2];
+
+console.log(array[0](2, 3));
+
+// Armazenar em um atributo de objeto
+const obj = {};
+obj.falar = function() { return 'Opa'; }
+console.log(obj.falar());
+
+// Passar funcao como param
+function run(fun) {
+    fun();
+}
+
+run(function() { console.log('Executando....') });
+
+// Uma funcao pode retornar/conter uma outra funcao
+function soma(a, b) {
+    return function(c) {
+        console.log(a + b + c);
+    }
+}
+
+soma(2, 3)(1);
+const somaMais = soma(2, 3);
+somaMais(4);
