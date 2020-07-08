0f2278e84ccbb7d8f609bcb1dd2281f84d0a8bbd
Guilherme Santos <g.santos.sfc@gmail.com>
2020-07-08 15:48:26 -0300

    add Aula Projeto API com Express 02 e 03

--- exercicios-js/node/projeto/src/servidor.js
+++ exercicios-js/node/projeto/src/servidor.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const bancoDeDados = require('./bancoDeDados');
+const bodyParser = require('body-parser');
 
 const port = 3003;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/produtos', (req, res) => {
-  res.send({ nome: 'Notebook', preco: 519.99 }); // automaticamente converte para JSON
+  res.send(bancoDeDados.getProdutos());
+});
+
+app.get('/produtos/:id', (req, res) => {
+  res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post('/produtos', (req, res) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco
+  });
+  res.send(produto);
+});
+
+app.put('/produtos/:id', (req, res) => {
+  const produto = bancoDeDados.salvarProduto({
+    id: Number(req.params.id),
+    nome: req.body.nome,
+    preco: req.body.preco
+  });
+  res.send(produto);
+});
+
+app.delete('/produtos/:id', (req, res) => {
+  const produto = bancoDeDados.excluirProduto(Number(req.params.id));
+  res.send(produto);
 });
 
 app.listen(port, () => {
